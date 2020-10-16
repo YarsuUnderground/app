aa32b580dfd008ff9acf0f8db97d1dd15eb4ac7c
Dragonite24 <qwez-9@mail.ru>
2020-10-17 01:52:52 +0300

    Update network.dart
    register, userType, MyId request

--- lib/client/network.dart
+++ lib/client/network.dart
@@ -23,4 +23,59 @@ Future<String> login(String login, String password) async {
   return token;
 }
 
+Future<String> register(String email, String password, String phone,
+    String first_name, String second_name) async {
+  Uri url = new Uri(
+    scheme: "https",
+    host: server,
+    path: "/register",
+    queryParameters: {
+      "email": "$email",
+      "password": "$password,",
+      "phone": "$phone",
+      "first name": "$first_name",
+      "second name": "$second_name"
+    },
+  );
+  var response = await http.get(url.toString());
+
+  dynamic data = json.decode(response.body);
+
+  String token = data["token"];
+
+  return token;
+}
+
+Future<bool> getUserType(String token) async {
+  Uri url = new Uri(
+    scheme: "https",
+    host: server,
+    path: "/getUserType",
+    queryParameters: {"token": "$token"},
+  );
+  var response = await http.get(url.toString());
+
+  dynamic data = json.decode(response.body);
+
+  bool isAdmin = data["token"];
+
+  return isAdmin;
+}
+
+Future<int> getMyId(String token) async {
+  Uri url = new Uri(
+    scheme: "https",
+    host: server,
+    path: "/getMyId",
+    queryParameters: {"token": "$token"},
+  );
+  var response = await http.get(url.toString());
+
+  dynamic data = json.decode(response.body);
+
+  int userId = data["token"];
+
+  return userId;
+}
+
 void main() async {}
