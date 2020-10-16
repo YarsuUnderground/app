dfa945fc82222164ebdd61dd602aee52c1033035
Vinogradov Alexander <astronom_35@mail.ru>
2020-10-17 01:32:51 +0300

    Create theme.dart

---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const mainBlue = MaterialColor(
+    0xFF5687EA,
+    {
+      900: Color(0xFF4344a2),
+      800: Color(0xFF4c64c3),
+      700: Color(0xFF5175d6),
+      600: Color(0xFF5687EA),
+      500: Color(0xFF5996f9),
+      400: Color(0xFF65a5fc),
+      300: Color(0xFF7ab5fd),
+      200: Color(0xFF9dcafe),
+      100: Color(0xFFc2deff),
+      50: Color(0xFFe6f2ff),
+    },
+  );
+}
