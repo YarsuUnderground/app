e91f20c6e5c1bcfca3b41c589a92cb4c764a244d
Vinogradov Alexander <astronom_35@mail.ru>
2020-10-17 18:22:12 +0300

    Fix pixel

--- lib/userScreen.dart
+++ lib/userScreen.dart
@@ -144,14 +144,14 @@ class _UserScreenState extends State<UserScreen> {
             children: [
               Container(
                 width: 1000,
-                height: 650,
+                height: 640,
                 child: Row(
                   children: [
                     Padding(
                       padding: EdgeInsets.all(20),
                       child: Container(
                         width: 460,
-                        height: 610,
+                        height: 600,
                         child: ListView(
                           children: [
                             _buildCard(
@@ -239,7 +239,7 @@ class _UserScreenState extends State<UserScreen> {
                       child: _buildCard(
                         child: Text("Новости"),
                         color: Colors.blue[200],
-                        height: 610,
+                        height: 600,
                         width: 460,
                         title: "Новости",
                         onPressed: () {},
