f8d4c3f49508c369fb9588d2947b27a8aa23054a
Vinogradov Alexander <astronom_35@mail.ru>
2020-10-17 11:37:07 +0300

    Create login.dart

---
+++ lib/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// TODO: Login page
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
