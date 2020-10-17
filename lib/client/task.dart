988ef4734a583eaf3ab1d7525ee0c0f09f6955d4
Vinogradov Alexander <astronom_35@mail.ru>
2020-10-17 14:49:52 +0300

    Добавил кооменты

--- lib/client/task.dart
+++ lib/client/task.dart
@@ -1,16 +1,38 @@
 class Task {
+  int id;
+
+  // id создателя
   int creatorId;
+
+  // название, описание
   String name, description;
+
+  // срок сдачи
   DateTime deadline;
+
   List<String> tags;
+
+  // исполнители
   List<int> performers;
   List<int> subtasks;
 }
 
 class SubTask {
+  int id;
+
   int creatorId;
+
   String name, description;
+
   DateTime deadline;
+
+  // исполнители
   List<int> performers;
+
+  /*
+  0 - не выполнено
+  1 - выполняется
+  2 - выполнено
+   */
   int status;
 }
