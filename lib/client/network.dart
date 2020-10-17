85000d63b4c04e23795d59e9dddc11af54d454c0
Vinogradov Alexander <astronom_35@mail.ru>
2020-10-17 14:25:06 +0300

    Переделано через GET

--- lib/client/network.dart
+++ lib/client/network.dart
@@ -3,100 +3,106 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'user.dart';
+import 'task.dart';
 
-const server = "dcrvosys.herokuapp.com";
-var client = http.Client();
+class NetworkClient {
+  static const server = "dcrvosys.herokuapp.com";
+  var client = http.Client();
+  Future<String> login(String login, String password) async {
+    Uri url = new Uri(
+      scheme: "https",
+      host: server,
+      path: "/auth/login/",
+      queryParameters: {"login": "$login", "password": "$password"},
+    );
+    var response = await http.get(url.toString());
 
-Future<String> login(String login, String password) async {
-  Uri url = new Uri(
-    scheme: "https",
-    host: server,
-    path: "/login",
-    queryParameters: {"login": "$login", "password": "$password,"},
-  );
-  var response = await http.get(url.toString());
+    dynamic data = json.decode(response.body);
 
-  dynamic data = json.decode(response.body);
+    String token = data["credentials"]["token"];
 
-  String token = data["token"];
+    return token;
+  }
 
-  return token;
+  Future<String> register(
+    String email,
+    String password,
+    String phone,
+    String first_name,
+    String second_name,
+  ) async {
+    Uri url = new Uri(
+      scheme: "https",
+      host: server,
+      path: "/auth/register/",
+      queryParameters: {
+        "email": "$email",
+        "password": "$password,",
+        "phone": "$phone",
+        "first_name": "$first_name",
+        "second_name": "$second_name"
+      },
+    );
+    var response = await http.get(url.toString());
+    dynamic data = json.decode(response.body);
+    String token = data["token"];
+    return token;
+  }
+
+  Future<bool> getUserType(String token) async {
+    Uri url = new Uri(
+      scheme: "https",
+      host: server,
+      path: "/getUserType",
+      queryParameters: {"token": "$token"},
+    );
+    var response = await http.get(url.toString());
+
+    dynamic data = json.decode(response.body);
+
+    bool isAdmin = data["token"];
+
+    return isAdmin;
+  }
+
+  Future<int> getMyId(String token) async {
+    Uri url = new Uri(
+      scheme: "https",
+      host: server,
+      path: "/getMyId",
+      queryParameters: {"token": "$token"},
+    );
+    var response = await http.get(url.toString());
+
+    dynamic data = json.decode(response.body);
+
+    int userId = data["token"];
+
+    return userId;
+  }
+
+  Future<User> getUser(int id) async {
+    Uri url = new Uri(
+      scheme: "https",
+      host: server,
+      path: "/getUser",
+      queryParameters: {"id": "$id"},
+    );
+    var response = await http.get(url.toString());
+
+    dynamic data = json.decode(response.body);
+
+    User user = User();
+    user.firstName = data["first_name"];
+    user.secondName = data["second_name"];
+    user.isAdmin = data["isAdmin"];
+    user.email = data["email"];
+    user.phone = data["phone"];
+
+    return user;
+  }
+
+  Future<Task> getTasks(int id) async {
+    return null;
+  }
 }
-
-Future<String> register(String email, String password, String phone,
-    String first_name, String second_name) async {
-  Uri url = new Uri(
-    scheme: "https",
-    host: server,
-    path: "/register",
-    queryParameters: {
-      "email": "$email",
-      "password": "$password,",
-      "phone": "$phone",
-      "first name": "$first_name",
-      "second name": "$second_name"
-    },
-  );
-  var response = await http.get(url.toString());
-
-  dynamic data = json.decode(response.body);
-
-  String token = data["token"];
-
-  return token;
-}
-
-Future<bool> getUserType(String token) async {
-  Uri url = new Uri(
-    scheme: "https",
-    host: server,
-    path: "/getUserType",
-    queryParameters: {"token": "$token"},
-  );
-  var response = await http.get(url.toString());
-
-  dynamic data = json.decode(response.body);
-
-  bool isAdmin = data["token"];
-
-  return isAdmin;
-}
-
-Future<int> getMyId(String token) async {
-  Uri url = new Uri(
-    scheme: "https",
-    host: server,
-    path: "/getMyId",
-    queryParameters: {"token": "$token"},
-  );
-  var response = await http.get(url.toString());
-
-  dynamic data = json.decode(response.body);
-
-  int userId = data["token"];
-
-  return userId;
-}
-
-Future<User> getUser(int id) async {
-  Uri url = new Uri(
-    scheme: "https",
-    host: server,
-    path: "/getUser",
-    queryParameters: {"id": "$id"},
-  );
-  var response = await http.get(url.toString());
-
-  dynamic data = json.decode(response.body);
-
-  User user = User();
-  user.firstName = data["first_name"];
-  user.secondName = data["second_name"];
-  user.isAdmin = data["isAdmin"];
-  user.email = data["email"];
-  user.phone = data["phone"];
-
-  return user;
-}
-
-void main() async {}
